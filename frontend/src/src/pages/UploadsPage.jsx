318c2eb9ca95a685346e3300d93558939f7d82e1
amiqua <tan48256@gmail.com>
2026-04-10 23:23:40 +0700

    chore(frontend): remove duplicated nested src directory

--- frontend/src/src/pages/UploadsPage.jsx
+++
@@ -1,5 +0,0 @@
-import UploadsPageContent from "@/components/uploads/UploadsPageContent";
-
-export default function UploadsPage({ onRequireLogin }) {
-  return <UploadsPageContent onRequireLogin={onRequireLogin} />;
-}
